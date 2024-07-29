48ab18e7753cc721b75cbbb6ebe9f8885be3af44
adamnikk <58403484+adamnik101@users.noreply.github.com>
2024-07-29 04:05:27 +0200

    track row remove from playlist; small header added with dominant color

---
+++ src/app/shared/pipes/format-date-from-now.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FormatDateFromNowPipe } from './format-date-from-now.pipe';
+
+describe('FormatDateFromNowPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FormatDateFromNowPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
